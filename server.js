2360f6738c94b1e327c3ca97960faac43f6d4a17
jaguuuar <sirkamson@gmail.com>
2020-05-01 17:52:38 +0200

    fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./src'));
+app.use(express.static('./dist/angular-bookstore'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('/index.html', {root: 'src/'}),
+    res.sendFile('index.html', {root: 'dist/angular-bookstore/'}),
 );
 
 app.listen(process.env.PORT || 8080);
